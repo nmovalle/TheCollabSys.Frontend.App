34e9cbeb1dcca43341447f8bfb6c38250681f968
nmovalle <11480516cii@gmail.com>
2024-03-28 21:44:58 -0600

    se agreg routerlink hacia clients component

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -23,7 +23,7 @@ export class AppMenuComponent implements OnInit {
             {
                 label: 'Employeers',
                 items: [
-                    { label: 'Clents', icon: 'pi pi-fw pi-id-card', routerLink: ['/employeers'] },
+                    { label: 'Clents', icon: 'pi pi-fw pi-id-card', routerLink: ['/clients'] },
                 ]
             },
             {
